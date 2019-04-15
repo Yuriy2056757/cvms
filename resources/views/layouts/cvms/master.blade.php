4cf6cdb3b517dbbc62c87b638ddbb00681d2599a
Yuriy2056757 <2056757@talnet.nl>
2019-04-15 21:17:51 +0200

    Update dashboard navigation layout

--- resources/views/layouts/cvms/master.blade.php
+++ resources/views/layouts/cvms/master.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html lang="nl">
+<html lang="en">
 
   <head>
     <meta charset="UTF-8">
@@ -25,15 +25,25 @@
             <div class="card mh-100 rounded-0">
               <div class="card-body p-0 mh-100">
                 <div id="sideNavLogo">
-                  <h5>CVMS dashboard</h5>
+                  <h5>CVMS DASHBOARD</h5>
                 </div>
 
                 <div class="sidenav-category">
-                  <h5 class="sidenav-category-title">Category</h5>
+                  <p class="sidenav-category-title">GENERAL</p>
 
                   <ul>
                     <li>
                       <a href="{{ route('home') }}">Home</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="sidenav-category">
+                  <p class="sidenav-category-title">CONTENT</p>
+
+                  <ul>
+                    <li>
+                      <a href="{{ route('home') }}">Curriculum Vitae</a>
                     </li>
                   </ul>
                 </div>
@@ -43,29 +53,22 @@
 
           {{-- Navbar --}}
           <div class="col p-0">
-            <nav class="navbar navbar-expand-lg navbar-dark primary-color position-absolute w-100">
-              <a class="navbar-brand" href="#">{{ ucfirst(Route::getCurrentRoute()->getName()) }}</a>
+            <nav class="navbar navbar-expand-lg navbar-dark indigo position-absolute w-100">
+                <span id="hamburger" class="navbar-toggler-icon rounded waves-effect waves-light"></span>
 
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
-                                                                                  aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
                 <ul class="navbar-nav ml-auto">
                   <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true"
-                                                                                                                aria-expanded="false">
-                      <i class="fa fa-user"></i> placeholder@email.com
+                    <a class="nav-link dropdown-toggle rounded" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true"
+                                                                                                             aria-expanded="false">
+                      <i class="fa fa-user"></i>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                      <a class="dropdown-item" href="#">Mijn account</a>
-                      <a class="dropdown-item" href="#">Uitloggen</a>
+                      <a class="dropdown-item" href="#">My account</a>
+                      <a class="dropdown-item" href="#">Logout</a>
                     </div>
                   </li>
                 </ul>
-              </div>
             </nav>
 
             <div class="container-fluid mh-100 offset-navigation">

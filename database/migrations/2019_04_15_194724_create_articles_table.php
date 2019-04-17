ba6fd980f405da7e519af08a810c008c5294e5ce
Yuriy2056757 <2056757@talnet.nl>
2019-04-17 23:24:44 +0200

    Add completed database migration for Article resource

--- database/migrations/2019_04_15_194724_create_articles_table.php
+++ database/migrations/2019_04_15_194724_create_articles_table.php
@@ -6,27 +6,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateArticlesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('article_name');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('articles', function (Blueprint $table) {
+      $table->bigIncrements('id')->unique()->index();
+      $table->foreign('user_id')->references('id')->on('users');
+      $table->string('article_name');
+      $table->string('slug')->index();
+      $table->string('header_title');
+      $table->text('body');
+      $table->string('seo_title', 70);
+      $table->string('meta_description', 150);
+      $table->boolean('is_active')->default(0);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('articles');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('articles');
+  }
 }

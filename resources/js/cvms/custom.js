67124592aa0adc260e16aec5d235c19977f68dc2
Yuriy2056757 <2056757@talnet.nl>
2019-06-09 14:05:56 +0200

    Toggle side navigation visibility on button press

---
+++ resources/js/cvms/custom.js
@@ -0,0 +1,8 @@
+alert('test');
+
+var sideNav = document.getElementById('sideNav');
+var button = document.getElementById('hamburgerContainer');
+button.onclick = function name() {
+    sideNav.style.setProperty('display', 'none', 'important');
+    alert('test');
+}
